74f5562a6dd5fc10d85cb3d36cde2291fc4cdc02
Aman Kumar <137664077+AMANkumar0004@users.noreply.github.com>
2026-03-04 21:44:16 +0530

    Fix CloudinaryStorage instantiation and formatting

--- cloudConfig.js
+++ cloudConfig.js
@@ -1,5 +1,5 @@
 const cloudinary = require('cloudinary').v2;
-const CloudinaryStorage = require('multer-storage-cloudinary');
+const { CloudinaryStorage } = require('multer-storage-cloudinary');
 
 cloudinary.config({
     cloud_name: process.env.CLOUD_NAME,
@@ -7,22 +7,11 @@ cloudinary.config({
     api_secret: process.env.CLOUD_API_SECRET,
 });
 
-<<<<<<< HEAD
-const storage = CloudinaryStorage({
-    cloudinary: cloudinary,
-    folder: 'wanderlust_DEV',
-    allowedFormats: ['png','jpg','jpeg'],
-  });
-
-
-  module.exports={
-=======
 const storage = new CloudinaryStorage({
->>>>>>> upstream/main
-    cloudinary,
+    cloudinary: cloudinary,
     params: {
         folder: 'wanderlust_DEV',
-        allowed_formats: ['png', 'jpg', 'jpeg'], // <-- FIXED
+        allowed_formats: ['png', 'jpg', 'jpeg'],
     },
 });
 
