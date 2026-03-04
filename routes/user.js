44053cf14317b2c59f6f7f7d9011f0f35bbab27b
Saptarshi Mukherjee <mukherjeesaptarshi289@gmail.com>
2026-03-04 15:33:05 +0530

    Enable booking functionality

--- routes/user.js
+++ routes/user.js
@@ -1,22 +1,24 @@
 const express = require("express");
-const router = express.Router({mergeParams:true});
+const router = express.Router({ mergeParams: true });
 const User = require("../models/user.js");
 const wrapAsync = require("../utils/wrapAsync");
 const passport = require("passport");
-const {saveRedirectUrl}= require("../middleware.js")
+const { saveRedirectUrl, isLoggedIn } = require("../middleware.js")
 
 const userController = require("../controllers/users.js")
 
 router.route("/signup")
-.get(userController.renderSignUpForm)
-.post(wrapAsync(userController.signUp));
+  .get(userController.renderSignUpForm)
+  .post(wrapAsync(userController.signUp));
 
 router.route("/login")
-.get(userController.renderLoginForm)
-.post(
-  saveRedirectUrl,
-  passport.authenticate("local",{failureRedirect:"/login",failureFlash:true}) ,userController.login);
-router.get("/logout",userController.logout)
+  .get(userController.renderLoginForm)
+  .post(
+    saveRedirectUrl,
+    passport.authenticate("local", { failureRedirect: "/login", failureFlash: true }), userController.login);
+router.get("/logout", userController.logout)
 
+router.route("/dashboard")
+  .get(isLoggedIn, wrapAsync(userController.renderDashboard));
 
-module.exports =router;
+module.exports = router;
